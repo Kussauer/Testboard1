8f12b3baea3b641dcedb08685091ea10dd4cf578
Kussauer <internet@nickkussauer.de>
2023-07-16 11:24:21 +0200

    delay -> millis umgestellt

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,36 @@
 #include <Arduino.h>
 
-int yellowLEDTest = 6;
-int button1 = 7;
-int buttonStatus = 0;
+int yellowLEDTest = 6; // Pin 6 - Board 1
+int button1 = 7;       // Pin 7 - Board 1
+int buttonStatus = 0; // Button 1 - HIGH oder LOW
 
-int testRun = 1;
+unsigned long previousMillis = 0;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200); // auch Konfig platformio.ini !!
   pinMode(yellowLEDTest, OUTPUT);
   pinMode(button1, INPUT);
 }
 
 void loop()
 {
+  unsigned long currentMillis = millis();
 
-  buttonStatus = digitalRead(button1);
+  if (currentMillis - previousMillis >= 200)
+  {
 
-  if (buttonStatus == HIGH)
-  {
-    digitalWrite(yellowLEDTest, HIGH);
-    Serial.println("Taster EIN");
-    delay(1000);
+    buttonStatus = digitalRead(button1);
+
+    if (buttonStatus == HIGH)
+    {
+      digitalWrite(yellowLEDTest, HIGH);
+      Serial.println("Taster EIN");
+      //delay(1000);
+    }
+    else
+    {
+      digitalWrite(yellowLEDTest, LOW);
+    }
   }
-  else
-  {
-    digitalWrite(yellowLEDTest, LOW);
-    Serial.println("Taster AUS");
-  }
-  
 }
